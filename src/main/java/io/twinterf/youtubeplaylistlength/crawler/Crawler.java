3519107248f0ab10f9582113897926b733ebc84a
ThomasW0221 <twinterf@uncc.edu>
2019-07-11 17:41:04 -0400

    add jsoup dependency and create crawler class

---
+++ src/main/java/io/twinterf/youtubeplaylistlength/crawler/Crawler.java
@@ -0,0 +1,11 @@
+package io.twinterf.youtubeplaylistlength.crawler;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Crawler {
+
+	public String run(String url) {
+		return null;
+	}
+}
