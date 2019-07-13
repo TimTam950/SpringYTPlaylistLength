b7ada5491eaf4809cddcd4f3e2193f98553a51c7
Thomas Winterfield <twinterf@uncc.edu>
2019-07-13 13:21:09 -0400

    add main in test class

--- src/main/java/io/twinterf/youtubeplaylistlength/crawler/Crawler.java
+++ src/main/java/io/twinterf/youtubeplaylistlength/crawler/Crawler.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+@Component
 public class Crawler {
 	
 	public String getLength(String url) {
