3908c0e4413e792cf828e835746f12a172d42fac
ThomasW0221 <twinterf@uncc.edu>
2019-07-22 17:49:42 -0400

    placeholder for scheduled task

--- src/main/java/io/twinterf/youtubeplaylistlength/YoutubePlaylistLengthApplication.java
+++ src/main/java/io/twinterf/youtubeplaylistlength/YoutubePlaylistLengthApplication.java
@@ -3,8 +3,10 @@ package io.twinterf.youtubeplaylistlength;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class YoutubePlaylistLengthApplication {
 
 	public static void main(String[] args) {
