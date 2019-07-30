767c4663e179b0aeede554ec4c55523da60e6ae3
ThomasW0221 <twinterf@uncc.edu>
2019-07-29 22:39:52 -0400

    move pattern out of method

--- src/main/java/io/twinterf/youtubeplaylistlength/crawler/Crawler.java
+++ src/main/java/io/twinterf/youtubeplaylistlength/crawler/Crawler.java
@@ -13,10 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Crawler {
 	
+	private Pattern p = Pattern.compile("([0-9]+):([0-9]{2})");
+	
 	public String getLength(String url) {
 		Document doc;
 		Elements elements = new Elements();
-		Pattern p = Pattern.compile("([0-9]+):([0-9]{2})");
 		ArrayList<Integer> minutes = new ArrayList<>();
 		ArrayList<Integer> seconds = new ArrayList<>();
 		try {
